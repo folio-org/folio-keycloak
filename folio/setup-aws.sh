7e4da30a3276442db9c3e58b0aabd2e25731e329
Kseniia Dubniak <46828141+kdubniak@users.noreply.github.com>
2024-08-29 00:05:51 +0300

    Update setup-aws.sh

--- folio/setup-aws.sh
+++ folio/setup-aws.sh
@@ -16,7 +16,8 @@ if [ -n "$OTEL_AGENT_EXTENSION_VERSION" ] && [ -n "$OTEL_AGENT_VERSION" ] && [ -
 
     aws s3 cp s3://$OTEL_BUCKET_NAME/$AGENT_EXTENSION_FOLDER/$AGENT_EXTENSION_FILE_NAME $AGENT_EXTENSION_PATH
     aws s3 cp s3://$OTEL_BUCKET_NAME/$AGENT_FILE_NAME $AGENT_PATH
-    JAVA_OPTS_APPEND="-javaagent:$AGENT_PATH -Dotel.javaagent.extensions=$AGENT_EXTENSION_PATH $JAVA_OPTS"
+    JAVA_OPTS_APPEND="-javaagent:$AGENT_PATH -Dotel.javaagent.extensions=$AGENT_EXTENSION_PATH $JAVA_OPTS_APPEND"
+    echo "$JAVA_OPTS_APPEND JAVA_OPTS_APPEND script"
   else
     echo "Opentelemetry java agent extension $OTEL_AGENT_EXTENSION_VERSION or java agent $OTEL_AGENT_VERSION not found in S3 bucket"
   fi
