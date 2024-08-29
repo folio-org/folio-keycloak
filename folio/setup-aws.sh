cbab02269b3872352d74535d9e0abafca28d8c0f
Kseniia Dubniak <46828141+kdubniak@users.noreply.github.com>
2024-08-29 08:58:13 +0300

    Update setup-aws.sh

--- folio/setup-aws.sh
+++ folio/setup-aws.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 # Check if OTEL_AGENT_EXTENSION_VERSION and OTEL_BUCKET_NAME environment variables are set
+echo "DEBUG STAGE"
+echo "$(aws s3 ls s3://$OTEL_BUCKET_NAME/$AGENT_EXTENSION_FOLDER/)"
+echo """$(aws s3 ls s3://$OTEL_BUCKET_NAME/ | grep "opentelemetry-javaagent")"""
+echo """$(aws s3 ls s3://$OTEL_BUCKET_NAME/$AGENT_EXTENSION_FOLDER/ | grep "$OTEL_AGENT_EXTENSION_VERSION" | awk '{print $NF}')"""
 if [ -n "$OTEL_AGENT_EXTENSION_VERSION" ] && [ -n "$OTEL_AGENT_VERSION" ] && [ -n "$OTEL_BUCKET_NAME" ]; then
   if [[ "$OTEL_AGENT_EXTENSION_VERSION" == *SNAPSHOT* ]]; then
     AGENT_EXTENSION_FOLDER="snapshots"
   else
     AGENT_EXTENSION_FOLDER="releases"
   fi
-  echo "$(aws s3 ls s3://$OTEL_BUCKET_NAME/$AGENT_EXTENSION_FOLDER/)"
-  echo """$(aws s3 ls s3://$OTEL_BUCKET_NAME/ | grep "opentelemetry-javaagent")"""
-  echo """$(aws s3 ls s3://$OTEL_BUCKET_NAME/$AGENT_EXTENSION_FOLDER/ | grep "$OTEL_AGENT_EXTENSION_VERSION" | awk '{print $NF}')"""
   AGENT_EXTENSION_FILE_NAME=$(aws s3 ls s3://$OTEL_BUCKET_NAME/$AGENT_EXTENSION_FOLDER/ | grep "$OTEL_AGENT_EXTENSION_VERSION" | awk '{print $NF}')
   AGENT_FILE_NAME=$(aws s3 ls s3://$OTEL_BUCKET_NAME/ | grep "opentelemetry-javaagent-$OTEL_AGENT_VERSION" | awk '{print $NF}')
   # If agent file found, copy it and add as Javaagent
