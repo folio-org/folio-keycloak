7aef3f372aa76bcb4fd7e9e05e9ef578895322d2
Kseniia Dubniak <46828141+kdubniak@users.noreply.github.com>
2024-08-29 09:03:54 +0300

    Update start.sh

--- folio/start.sh
+++ folio/start.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 # Wrapper script as docker entrypoint to run configure-realms.sh in parallel to actual kc.sh (the official entrypoint).
+#!/bin/bash
+# Check if OTEL_AGENT_EXTENSION_VERSION and OTEL_BUCKET_NAME environment variables are set
+echo "DEBUG STAGE"
+echo "$(aws s3 ls s3://$OTEL_BUCKET_NAME/$AGENT_EXTENSION_FOLDER/)"
+echo """$(aws s3 ls s3://$OTEL_BUCKET_NAME/ | grep "opentelemetry-javaagent")"""
+echo """$(aws s3 ls s3://$OTEL_BUCKET_NAME/$AGENT_EXTENSION_FOLDER/ | grep "$OTEL_AGENT_EXTENSION_VERSION" | awk '{print $NF}')"""
+if [ -n "$OTEL_AGENT_EXTENSION_VERSION" ] && [ -n "$OTEL_AGENT_VERSION" ] && [ -n "$OTEL_BUCKET_NAME" ]; then
+  if [[ "$OTEL_AGENT_EXTENSION_VERSION" == *SNAPSHOT* ]]; then
+    AGENT_EXTENSION_FOLDER="snapshots"
+  else
+    AGENT_EXTENSION_FOLDER="releases"
+  fi
+  AGENT_EXTENSION_FILE_NAME=$(aws s3 ls s3://$OTEL_BUCKET_NAME/$AGENT_EXTENSION_FOLDER/ | grep "$OTEL_AGENT_EXTENSION_VERSION" | awk '{print $NF}')
+  AGENT_FILE_NAME=$(aws s3 ls s3://$OTEL_BUCKET_NAME/ | grep "opentelemetry-javaagent-$OTEL_AGENT_VERSION" | awk '{print $NF}')
+  # If agent file found, copy it and add as Javaagent
+  if [ -n "$AGENT_EXTENSION_FILE_NAME" ] && [ -n "$AGENT_FILE_NAME" ]; then
+    AGENT_PATH="/opt/javaagents/$AGENT_FILE_NAME"
+    AGENT_EXTENSION_PATH="/opt/javaagents/$AGENT_EXTENSION_FILE_NAME"
+
+    aws s3 cp s3://$OTEL_BUCKET_NAME/$AGENT_EXTENSION_FOLDER/$AGENT_EXTENSION_FILE_NAME $AGENT_EXTENSION_PATH
+    aws s3 cp s3://$OTEL_BUCKET_NAME/$AGENT_FILE_NAME $AGENT_PATH
+    JAVA_OPTS_APPEND="-javaagent:$AGENT_PATH -Dotel.javaagent.extensions=$AGENT_EXTENSION_PATH $JAVA_OPTS_APPEND"
+    echo "$JAVA_OPTS_APPEND JAVA_OPTS_APPEND script"
+  else
+    echo "Opentelemetry java agent extension $OTEL_AGENT_EXTENSION_VERSION or java agent $OTEL_AGENT_VERSION not found in S3 bucket"
+  fi
+else
+  echo "OTEL_AGENT_EXTENSION_VERSION environment variable is not set"
+fi
+
 echo "$JAVA_OPTS_APPEND JAVA_OPTS_APPEND"
 if [[ -z "$KC_FOLIO_BE_ADMIN_CLIENT_SECRET" ]]; then
   echo "$(date +%F' '%T,%3N) ERROR [start.sh] Environment variable KC_FOLIO_BE_ADMIN_CLIENT_SECRET is not set, check 
