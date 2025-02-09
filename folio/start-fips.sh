6310fcb3de6b84d78e127d9d151f5eb89e6ab0e6
Max Sinichenko <exel711@gmail.com>
2025-02-09 20:13:03 +0200

    Update start-fips.sh

--- folio/start-fips.sh
+++ folio/start-fips.sh
@@ -21,6 +21,7 @@ echo "Starting in FIPS mode"
  --https-key-store-file="${KC_HTTPS_KEY_STORE:-/opt/keycloak/conf/test.server.keystore}" \
  --https-key-store-password=${KC_HTTPS_KEY_STORE_PASSWORD:-SecretPassword} \
  --spi-password-hashing-pbkdf2-sha256-max-padding-length=14 \
+ --spi-password-hashing-pbkdf2-sha512-max-padding-length=14 \
  --cache="$kcCache" \
  --cache-config-file="$kcCacheConfigFile" \
  --log-level="${KC_LOG_LEVEL:-${logLevel}}" \
